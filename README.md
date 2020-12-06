34376204113cc4ef4576000e580a30e9f6dabe83
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-12-06 17:09:10 +0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # flask-react-blank
+
+React app with react frontend
+
+###Develop
+####Backend
+cd *this project*
+
+> python main.py
+
+####Frontend
+cd ./frontend
+
+> npm run dev
---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__,
+            template_folder='frontend/templates',
+            static_folder='frontend/static')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
